c4277fd32d4947383fa6f90eb577ccffd18d15cb
Bilguun.Leaf-3 <bilguun.leaf-3@KEKW.local>
2023-04-11 11:37:02 +0800

    Making register section

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -8,7 +8,7 @@ export const Layout: FC<PropsWithChildren> = ({ children }) => {
   }, [loginModal])
   return (
     <>
-        <div className='w-full py-4 px-16 absolute flex items-center justify-between bg-black border-b'>
+        <div className='w-full py-4 px-16 absolute flex items-center justify-between bg-black border-b z-10'>
             <button 
               className='text-white text-xl hover:text-slate-200 active:text-slate-300 transition'
               onClick={logout}
